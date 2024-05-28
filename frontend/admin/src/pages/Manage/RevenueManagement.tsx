4095ade4f86d737c9bae798c1ef11475812b1ff4
Nguyễn Hữu Đạt <20130224@st.hcmuaf.edu.vn>
2024-05-28 17:06:28 +0700

    update table ver2

--- frontend/admin/src/pages/Manage/RevenueManagement.tsx
+++ frontend/admin/src/pages/Manage/RevenueManagement.tsx
@@ -10,7 +10,7 @@ const RevenueManagement: React.FC = () => {
             <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
                 <ChartOne />
                 <ChartThree />
-                <div className="col-span-12 xl:col-span-8">
+                <div className="col-span-12">
                     <TableOne />
                 </div>
             </div>
--- frontend/admin/src/pages/Manage/VoucherManagement.tsx
+++ frontend/admin/src/pages/Manage/VoucherManagement.tsx
@@ -9,7 +9,7 @@ const WarehouseManagement: React.FC = () => {
             <div className="flex flex-col gap-10">
                 <Link
                     to="#"
-                    className="inline-flex items-center justify-center gap-2.5 rounded-md bg-primary py-3 px-2 text-center font-medium text-white hover:bg-opacity-90 w-full lg:w-1/2 xl:w-1/5"
+                    className="inline-flex items-center justify-center gap-2.5 rounded-md bg-primary py-3 px-2 text-center font-medium text-white hover:bg-opacity-90 w-full lg:w-1/4 xl:w-1/5"
                 >
                     <span>
                         <i className="fa-sharp fa-solid fa-plus"></i>
