f8e0e94d3ce12a713f3607637aee6009a36f09da
Nguyễn Hữu Đạt <20130224@st.hcmuaf.edu.vn>
2024-05-30 18:26:13 +0700

    update backend ver1

---
+++ backend/src/test/java/vn/edu/hcmuaf/fit/backend/eCommerceRestfulAPI/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.fit.backend.eCommerceRestfulAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
