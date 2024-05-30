b1efa16d795e7d446cf50709a51852a4bcd7df64
Nguyễn Hữu Đạt <20130224@st.hcmuaf.edu.vn>
2024-05-30 18:34:44 +0700

    update backend ver2

---
+++ backend/eCommerceRestfulAPI/src/main/java/vn/edu/hcmuaf/fit/ecommercerestfulapi/ECommerceRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.fit.ecommercerestfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceRestfulApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceRestfulApiApplication.class, args);
+    }
+
+}
---
+++ backend/eCommerceRestfulAPI/src/test/java/vn/edu/hcmuaf/fit/ecommercerestfulapi/ECommerceRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.fit.ecommercerestfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceRestfulApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
