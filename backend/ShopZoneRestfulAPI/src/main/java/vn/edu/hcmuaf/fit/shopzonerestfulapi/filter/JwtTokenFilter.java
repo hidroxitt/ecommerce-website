fe882875efde59b78974fb3d8a2545ae238fce94
Nguyễn Hữu Đạt <20130224@st.hcmuaf.edu.vn>
2024-06-09 20:20:09 +0700

    update auth

--- backend/ShopZoneRestfulAPI/src/main/java/vn/edu/hcmuaf/fit/shopzonerestfulapi/filter/JwtTokenFilter.java
+++
@@ -1,4 +0,0 @@
-package vn.edu.hcmuaf.fit.shopzonerestfulapi.filter;
-
-public class JwtTokenFilter {
-}
--- backend/ShopZoneRestfulAPI/src/main/java/vn/edu/hcmuaf/fit/shopzonerestfulapi/security/SecurityConfig.java
+++ backend/ShopZoneRestfulAPI/src/main/java/vn/edu/hcmuaf/fit/shopzonerestfulapi/security/SecurityConfig.java
@@ -1,9 +1,17 @@
 package vn.edu.hcmuaf.fit.shopzonerestfulapi.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return null;
+    }
 }
